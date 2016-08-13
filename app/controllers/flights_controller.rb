c6414b47e5747144feecf2f99e1b17fde647eac5
Anna Krupp <email": null>
2016-08-12 17:12:12 -0700

    Pseudo-code generate flights method and template draft search_results show
      - Add template search_results that would display a list of flights
      - Add pseudo-code of the generate method to search form model
      - Add show method to flights controller to cover edge case when user refreshes the search results page

--- app/controllers/flights_controller.rb
+++ app/controllers/flights_controller.rb
@@ -6,9 +6,19 @@ class FlightsController < ApplicationController
 
   def search_results
     @search = SearchForm.new(search_params)
-
-    @flights = @search.search
+    if @search.valid?
+      @flights = @search.generate_flights
+    else
+      #rerender the search template
+      render :search
+    end
   end
+
+  def show
+    # if the user refreshes the search/results page
+    redirect_to '/search'
+  end
+
   private
   def search_params
     params.require(:search_form).permit(:location, :beverage, :distance)
--- app/models/search_form.rb
+++ app/models/search_form.rb
@@ -17,6 +17,41 @@ class SearchForm # https://robots.thoughtbot.com/activemodel-form-objects
 
   validates_presence_of :location, :beverage, :distance
 
+  # def get_biz_within_distance
+    # Business.near(self.location, self.distance)
+  # end
+
+  # def get_themed_biz(biz_collection)
+  # Smth like this.
+    # case self.beverage
+    # case 'Wine'
+      # biz_collection.wine
+    # case 'Beer'
+      # biz_collection.beer
+    # case 'Whiskey'
+      # biz_collection.whiskey
+    # case 'Coffee'
+      # biz_collection.coffee
+    # end
+  # end
+
+  def generate_flights
+#returns a collection of flights based on search params
+
+# Steps:
+# get businesses within the given distance
+    # biz_within_distance = get_biz_within_distance(possible_flights)
+# get the list of businesses that have needed beverage category
+    # themed_biz_collection = search.get_themed_biz(biz_within_distance)
+# rank the businesses by the ratings
+
+# select 5 leading businesses
+
+# create 5 flights with 5 businesses inside a flight around the leading business
+
+    []
+  end
+
   private
   def self.distance_array(min, max)
     distance_array = [0, 2.5, 5, 7.5]
