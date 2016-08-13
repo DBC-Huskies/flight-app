854f5979757c475d362d1a68e30aff0fba5517ed
Andrew Peterson <email": null>
2016-08-12 17:04:21 -0700

    Add tests for enum theme key value pairs. Tests green.

--- spec/models/business_spec.rb
+++ spec/models/business_spec.rb
@@ -1,10 +1,10 @@
 require 'rails_helper'
 
 describe 'Business' do
-  let(:valid_bus) { Business.new(name: "Whiskey Bar", location: "Ballard") }
-  let(:invalid_bus) { Business.new(name: "", location: "") }
-
   describe 'validations' do
+    let(:valid_bus) { Business.new(name: "Whiskey Bar", location: "Ballard") }
+    let(:invalid_bus) { Business.new(name: "", location: "") }
+
     describe 'when valid' do
       it 'validates the presence of name' do
         expect(valid_bus.name).to eq('Whiskey Bar')
@@ -22,6 +22,26 @@ describe 'Business' do
     describe 'when not valid' do
       it 'is not valid without a name and location' do
         expect(invalid_bus).to_not be_valid
+      end
+    end
+  end
+
+  describe 'enum' do
+    describe 'theme associations' do
+      it 'validates whiskey is at position 2 in the theme enum' do
+        expect(Business.themes[:whiskey]).to eq 2
+      end
+
+      it 'validates whiskey is at position 0 in the theme enum' do
+        expect(Business.themes[:wine]).to eq 0
+      end
+
+      it 'validates whiskey is at position 1 in the theme enum' do
+        expect(Business.themes[:beer]).to eq 1
+      end
+
+      it 'validates whiskey is at position 3 in the theme enum' do
+        expect(Business.themes[:coffee]).to eq 3
       end
     end
   end
