0373f86c923ca431dc1338d32785ccef11f240eb
Jacob Rapp <email": null>
2016-08-15 16:58:12 -0700

    Add links to header, add routes for session and user

--- config/routes.rb
+++ config/routes.rb
@@ -6,4 +6,6 @@ Rails.application.routes.draw do
     post 'search_results', on: :collection
   end
 
+  resources :users, only: [:new, :create, :show]
+  resources :sessions, only: [:new, :create, :destroy]
 end
