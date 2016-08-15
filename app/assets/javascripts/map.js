b06ba2d91e04fd25060c01207c354fd7d9d1d119
Anna Krupp <cherchezlafemme@users.noreply.github.com>
2016-08-14 17:37:48 -0700

    Working on event listener that catches resizing of the screen and adjusts the width of the business list. Still in progress. Not completed

--- app/assets/javascripts/map.js
+++ app/assets/javascripts/map.js
@@ -1,9 +1,27 @@
 $( document ).ready(function() {
-    var heights = $("#business-list").map(function() {
-        return $(this).height();
-    }).get(),
+  var heights = $("#business-list").map(function() {
+    return $(this).height();
+  }).get(),
 
-    maxHeight = Math.max.apply(null, heights);
+  maxHeight = Math.max.apply(null, heights);
 
-    $("#map-canvas").height(maxHeight);
+  $("#map-canvas").height(maxHeight);
+
+  function checkWidth() {
+      var containerSize = $('.container').width();
+      if (containerSize <= 720) {
+          debugger;
+          //if the container is smaller then 720 px
+          //then change the width of the business list 
+          $('#business-list').width(mapWidth);
+
+      }
+  }
+    // Execute on load
+  checkWidth();
+    // add event listener
+  $('.container').resize(checkWidth);
+
 });
+
+  
