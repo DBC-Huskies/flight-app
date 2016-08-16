f4ff79db566b4976753913f7bf9d7873bd686085
Jacob Rapp <email": null>
2016-08-16 12:25:52 -0700

    Building Session Controller spec, lunch break

--- spec/controllers/sessions_controller_spec.rb
+++ spec/controllers/sessions_controller_spec.rb
@@ -1,6 +1,27 @@
 require 'rails_helper'
 
 describe SessionsController do
-  xit 'does a thing' do
+
+  it 'renders the login page' do
+    get :new
+    expect(response).to have_http_status(:ok)
+    expect(response).to render_template(:new)
   end
+
+  it 'assigns an empty login form' do
+    get :new
+    expect(assigns(:login)).to be_a(LoginForm)
+  end
+
+  describe '#destroy' do
+    before(:each) do
+      session[:user_id] = true
+      get :destroy
+    end
+
+    it 'sets the session id to nil' do
+      expect(session[:user_id]).to eq false
+    end
+  end
+
 end
