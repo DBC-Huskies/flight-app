0aa7498456a167621908a4aa4b4d64a26342d3e8
Jacob Rapp <email": null>
2016-08-16 10:40:38 -0700

    Add pending tests to every spec file to ensure we come back to it

---
+++ spec/controllers/sessions_controller_spec.rb
@@ -0,0 +1,6 @@
+require 'rails_helper'
+
+describe SessionsController do
+  xit 'does a thing' do
+  end
+end
---
+++ spec/controllers/users_controller_spec.rb
@@ -0,0 +1,6 @@
+require 'rails_helper'
+
+describe UsersController do
+  xit 'does a thing' do
+  end
+end
