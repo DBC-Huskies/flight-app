9aa53d75d933e0b444588959bd08f41abc066ef1
Jacob Rapp <email": null>
2016-08-16 15:15:11 -0700

    Tests for session controller destroy action

--- spec/controllers/sessions_controller_spec.rb
+++ spec/controllers/sessions_controller_spec.rb
@@ -14,14 +14,35 @@ describe SessionsController do
   end
 
   describe '#destroy' do
+
     before(:each) do
-      session[:user_id] = true
-      get :destroy
+      session[:user_id] = 54
+      delete :destroy, id: 54
     end
 
     it 'sets the session id to nil' do
-      expect(session[:user_id]).to eq false
+      expect(session[:user_id]).to eq nil
     end
+
+    it 'redirects to the search page' do
+      expect(response).to have_http_status(:redirect)
+      expect(response).to redirect_to(search_flights_path)
+    end
+
+  end
+
+  describe '#create' do
+    let(:valid_user) { User.create(username: "Jimmy Dean", password: '5au5ag3l1nk')}
+
+    context 'with valid login credentials' do
+      post :create, login_form: {:username => "Jimmy Dean", :password => "5au5ag3l1nk"}
+      expect(response).to have_http_status(:ok)
+    end
+
+    context 'with invalid login credentials' do
+
+    end
+
   end
 
 end
