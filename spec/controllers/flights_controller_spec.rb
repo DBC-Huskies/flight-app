254f6c5ff3111edb6599be1ab67546584bed24cd
Jacob Rapp <email": null>
2016-08-16 16:59:03 -0700

    One test pending on flight controller - AJAX search form error handling

--- spec/controllers/flights_controller_spec.rb
+++ spec/controllers/flights_controller_spec.rb
@@ -8,7 +8,7 @@ describe FlightsController do
       get :index
     end
 
-    it 'redirects to search_results_flights_path and responds with a status of 302' do
+    it 'redirects to search results' do
       expect(response.status).to eq(302)
     end
 
@@ -50,15 +50,27 @@ describe FlightsController do
 
     describe 'HTTP requests' do
 
-      before(:each) do
-        get :search_results
-      end
-
       context 'with valid data' do
+
+        before(:each) do
+          post :search_results, search_form: {location: "Seattle, WA", beverage: "Beer", distance: 5}
+        end
+
+        it 'renders the results page' do
+          expect(response).to render_template :search_results
+        end
 
       end
 
       context 'with invalid data' do
+
+        before(:each) do
+          post :search_results, search_form: {location: "", beverage: "Beer", distance: 5}
+        end
+
+        it 'render the search form' do
+          expect(response).to render_template :search
+        end
 
       end
 
@@ -66,15 +78,27 @@ describe FlightsController do
 
     describe 'AJAX requests' do
 
-      before(:each) do
-        xhr :post, :search_results
-      end
-
       context 'with valid data' do
+
+        before(:each) do
+          xhr :post, :search_results, search_form: {location: "Seattle, WA", beverage: "Beer", distance: 5}
+        end
+
+        it 'renders the results page' do
+          expect(response).to render_template :search_results
+        end
 
       end
 
       context 'with invalid data' do
+
+        before(:each) do
+          xhr :post, :search_results, search_form: {location: "", beverage: "Beer", distance: 5}
+        end
+
+        xit 'returns errors for the search form' do
+          expect(response).to render_template :search
+        end
 
       end
 
@@ -92,20 +116,22 @@ describe FlightsController do
         get :show, id: new_flight.id
       end
 
-      it '' do
-
+      it 'redirects to the flight page' do
+        expect(response).to render_template :show
       end
 
     end
 
     describe 'AJAX requests' do
+      render_views
 
       before(:each) do
         xhr :get, :show, id: new_flight.id
       end
 
-      it '' do
-
+      it 'renders the flight show page' do
+        expect(response).to render_template :show
+        expect(response.body).to match /[container]/
       end
 
     end
