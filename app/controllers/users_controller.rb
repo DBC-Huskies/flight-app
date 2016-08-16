bce557e84aeb59c84a45a85b88d790eb46b0c147
Jacob Rapp <email": null>
2016-08-16 07:53:01 -0700

    User can login

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -5,14 +5,16 @@ class UsersController < ApplicationController
 
   def create
     @user = User.create(get_user_params)
-    # if @user.valid?
-    #   redirect_to user_path
-    # else
-    #   render :'users/new'
-    # end
+    if @user.valid?
+      session[:user_id] = @user.id
+      redirect_to user_path(@user)
+    else
+      render :'users/new'
+    end
   end
 
   def show
+    @user = User.find(params[:id])
   end
 
   private
