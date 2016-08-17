138b53cd313bdbb7ddc86dedd824210d0edf3f1e
Jacob Rapp <email": null>
2016-08-17 10:31:23 -0700

    Custom error message displays if user attempts to rate the same businesss twice

--- app/controllers/ratings_controller.rb
+++ app/controllers/ratings_controller.rb
@@ -7,7 +7,7 @@ class RatingsController < ApplicationController
     @rating = Rating.new(rating_params)
     @rating.business = Business.find(params['business_id'])
     @rating.author = User.find(session[:user_id])
-    if @rating.save!
+    if @rating.save
       session[:flash] = "Thank you for submitting a rating!"
       redirect_to user_path(session[:user_id])
     else
