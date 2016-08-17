cc4327347fe84b65e74ccaa4978a3331d318f0e1
Jacob Rapp <email": null>
2016-08-16 17:53:15 -0700

    Minimal additions to Search_form_spec

--- spec/models/search_form_spec.rb
+++ spec/models/search_form_spec.rb
@@ -2,8 +2,6 @@ require 'rails_helper'
 
 describe SearchForm do
 
-  let(:search) { SearchForm.new(location: '83 S King St., Seattle, WA', beverage: 'Beer', distance: 5) }
-
   it 'has a collection of beverages for the form' do
     expect(SearchForm.beverage_options).to eq ['Wine', 'Beer', 'Whiskey', 'Coffee']
   end
@@ -12,12 +10,25 @@ describe SearchForm do
     expect(SearchForm.distance_options).to include 35
   end
 
-
+  # it 'returns the correct enum value from private method' do
+  #   result = SearchForm.send(:get_theme_enum, 'Wine')
+  #   expect(result).to eq 1
+  # end
 
   describe '#generate_flights' do
 
-    it "returns a flight of businesses" do
-      expect(search.generate_flights).to be_an Array
+    let(:beer_search) { SearchForm.new(location: '83 S King St., Seattle, WA', beverage: 'Beer', distance: 5) }
+    let(:wine_search) { SearchForm.new(location: '83 S King St., Seattle, WA', beverage: 'Wine', distance: 5) }
+    let(:whiskey_search) { SearchForm.new(location: '83 S King St., Seattle, WA', beverage: 'Whiskey', distance: 5) }
+    let(:coffee_search) { SearchForm.new(location: '83 S King St., Seattle, WA', beverage: 'Coffee', distance: 5) }
+
+    let(:beer_collection) { beer_search.generate_flights }
+    let(:wine_collection) { wine_search.generate_flights }
+    let(:whiskey_collection) { whiskey_search.generate_flights }
+    let(:coffee_collection) { coffee_search.generate_flights }
+
+    it 'returns an array' do
+      expect(beer_collection).to be_an Array
     end
 
   end
