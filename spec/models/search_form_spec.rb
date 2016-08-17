b4d42a54eff1334119e45dbd780091cd448a8cb7
Jacob Rapp <email": null>
2016-08-16 17:09:31 -0700

    Add two test to search_form spec

--- spec/models/search_form_spec.rb
+++ spec/models/search_form_spec.rb
@@ -4,9 +4,19 @@ describe SearchForm do
 
   let(:search) { SearchForm.new(location: '83 S King St., Seattle, WA', beverage: 'Beer', distance: 5) }
 
+  it 'has a collection of beverages for the form' do
+    expect(SearchForm.beverage_options).to eq ['Wine', 'Beer', 'Whiskey', 'Coffee']
+  end
+
+  it 'has a collection of distances' do
+    expect(SearchForm.distance_options).to include 35
+  end
+
+
+
   describe '#generate_flights' do
 
-    it "return a flight of businesses" do
+    it "returns a flight of businesses" do
       expect(search.generate_flights).to be_an Array
     end
 
