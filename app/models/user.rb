65e5ed2849d6f6e82812544397153946605e35cd
Andrew Peterson <email": null>
2016-08-18 11:11:02 -0700

    Finish all form styling and error messages.

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,8 +2,9 @@ class User < ActiveRecord::Base
   has_many :ratings, foreign_key: 'author_id'
   has_and_belongs_to_many :flights
 
-  validates :username, presence: true, uniqueness: true
+  validates_presence_of :username, uniqueness: true, allow_blank: false, message: "Enter a username."
   validates :password, length: { :minimum => 4}
+  validates_presence_of :password_confirmation, allow_blank: false, message: "Confirm your password."
 
   has_secure_password
 end
